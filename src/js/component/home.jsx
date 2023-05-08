4eb7b6a11d67a85406c1b5d3a0c7db8242b9d650
julitar <126813769+julitar@users.noreply.github.com>
2023-05-08 19:44:06 +0000

    Un poco de Bootstrap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,27 +6,33 @@ import React, {useState} from "react";
 //create your first component
 const Home = () => {
 
-	const [ selectedColor, setSelectedColor] = useState("green")
+	const [ color, setColor] = useState("")
 
 	return (
-		<div className="text-center">
-
-			<div className="top">
+		<div clasName="container-fluid">
+		<div className="row my-5">
+      		<div className="col-sm-12">
+          		<div className="alert alert-warning text-center" role="alert">
+            	<h2>This is a traffic light</h2>
+          		</div>
+        	</div>
+		</div>
+		
+		<div className="row my-5">
+			<div className="col-sm-12 d-flex justify-content-center">
+				<div className="cuerpo">
+					<div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow" : "")}></div>
+					<div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow" : "")}> </div>
+					<div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow" : "")}></div>
+				</div>
 			</div>
+		</div>
 
-			<div className="cuerpo">
-				
-				<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
-				<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}> </div>
-				<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
-			
-			</div>
-			
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row my-5">
+			<div className="fixed-bottom p-4 text-danger text-center">
+        		Made with ❤️ by <b>Julia</b>
+      		</div>
+		</div>
 		</div>
 	);
 };
