d036776f916c57690d881b807985df36e8cf7cce
julitar <126813769+julitar@users.noreply.github.com>
2023-05-08 18:59:53 +0000

    useState por colores asociado a la clase glow

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,18 +5,21 @@ import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+
+	const [ selectedColor, setSelectedColor] = useState("green")
+
 	return (
 		<div className="text-center">
-			
+
 			<div className="top">
 			</div>
-			
-			<div className="cuerpo">
-				<div className="light red glow"></div>
-				<div className="light yellow glow"></div>
-				<div className="light green glow"></div>
-			</div>
 
+			<div className="cuerpo">
+				<div className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+				<div className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}> </div>
+				<div className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
+			</div>
+			
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
